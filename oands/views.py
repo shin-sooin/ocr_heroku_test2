e01b569df6300230117d32860c6e01a29adc9dbb
shin-sooin <64197786+shin-sooin@users.noreply.github.com>
2022-12-12 02:42:36 +0900

    only request

--- oands/views.py
+++ oands/views.py
@@ -12,13 +12,13 @@ import os
 # import Image from PIL to read image
 from PIL import Image
 from django.conf import settings
-from django.http import HttpResponse, JsonResponse, request
+from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
 import urllib.request
 from io import BytesIO
 from PIL import Image
-from django.http import HttpRequest as rq
+#from django.http import HttpRequest as rq
 
 import time
 #file='https://firebasestorage.googleapis.com/v0/b/ocr-test2-9b5be.appspot.com/images%2Fexxx.png?alt=media'
@@ -29,7 +29,7 @@ import sys
 
 # connect with firebase
 @csrf_exempt
-def index(request: rq):
+def index(request):
     text = ""
     message = ""
     eng_to_kor=""
@@ -47,43 +47,43 @@ def index(request: rq):
                 #print(requests.data['image'])
                 request_msg=request.data['image']
 
-                img=img_open(request.data['image'])
-                text = pytesseract.image_to_string(img, lang='kor+eng')
-                text = text.encode("ascii", "ignore")
-                text = text.decode()
+                # img=img_open(request.data['image'])
+                # text = pytesseract.image_to_string(img, lang='kor+eng')
+                # text = text.encode("ascii", "ignore")
+                # text = text.decode()
 
-                # translate eng to kor through Papago API
-                client_id = "7cyuDLUY3kSNzmFs_i88" # 개발자센터에서 발급받은 Client ID 값
-                client_secret = "NMYcZYMSNp" # 개발자센터에서 발급받은 Client Secret 값
-                encText = urllib.parse.quote(text)
-                data = "source=en&target=ko&text=" + encText
-                url = "https://openapi.naver.com/v1/papago/n2mt"
-                request = urllib.request.Request(url)
-                request.add_header("X-Naver-Client-Id",client_id)
-                request.add_header("X-Naver-Client-Secret",client_secret)
-                response = urllib.request.urlopen(request, data=data.encode("utf-8"))
-                rescode = response.getcode()
-
-                if(rescode==200):
-                    response_body = response.read()
-                    result=response_body.decode('utf-8')
-                    d=json.loads(result)
-                    eng_to_kor = d['message']['result']['translatedText']
-                    # print(response_body)
-                    # print(eng_to_kor)
-                else:
-                    eng_to_kor = "error code: "+rescode
-
-                # Summary (0.1% of the original content).
-                # summarized_text = summarizer.summarize('',text, count=2)
-                # os.remove(pathz)
+            #     # translate eng to kor through Papago API
+            #     client_id = "7cyuDLUY3kSNzmFs_i88" # 개발자센터에서 발급받은 Client ID 값
+            #     client_secret = "NMYcZYMSNp" # 개발자센터에서 발급받은 Client Secret 값
+            #     encText = urllib.parse.quote(text)
+            #     data = "source=en&target=ko&text=" + encText
+            #     url = "https://openapi.naver.com/v1/papago/n2mt"
+            #     request = urllib.request.Request(url)
+            #     request.add_header("X-Naver-Client-Id",client_id)
+            #     request.add_header("X-Naver-Client-Secret",client_secret)
+            #     response = urllib.request.urlopen(request, data=data.encode("utf-8"))
+            #     rescode = response.getcode()
+            #
+            #     if(rescode==200):
+            #         response_body = response.read()
+            #         result=response_body.decode('utf-8')
+            #         d=json.loads(result)
+            #         eng_to_kor = d['message']['result']['translatedText']
+            #         # print(response_body)
+            #         # print(eng_to_kor)
+            #     else:
+            #         eng_to_kor = "error code: "+rescode
+            #
+            #     # Summary (0.1% of the original content).
+            #     # summarized_text = summarizer.summarize('',text, count=2)
+            #     # os.remove(pathz)
             except:
                 message = "check your filename and ensure it doesn't have any space or check if it has any text"
 
     context = {
-        'text': text,
-        'message': message,
-        'eng_to_kor': eng_to_kor,
+        # 'text': text,
+        # 'message': message,
+        # 'eng_to_kor': eng_to_kor,
         'request_msg': request_msg
     }
     # get(context)
@@ -92,9 +92,9 @@ def index(request: rq):
 # def get(context):
 #     return JsonResponse(context)
 
-def img_open(imgUrl):
-    # request.urlopen()
-    res = request.urlopen(imgUrl).read()
-    # Image open
-    img = Image.open(BytesIO(res))
-    return img
+# def img_open(imgUrl):
+#     # request.urlopen()
+#     res = request.urlopen(imgUrl).read()
+#     # Image open
+#     img = Image.open(BytesIO(res))
+#     return img
